eb12ad76868d351067891fc481c1ab8493be6f82
dlrandy <1208484996@qq.com>
2021-08-19 23:30:14 +0800

    chore: stash

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 import './index.css'
 import 'virtual:windi.css'
 import App from './App'
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
